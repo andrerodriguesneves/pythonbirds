fd205d319a483763e87aa7e72a337d11a0f06e4b
andrerodriguesneves <andre.rodriguesneves@gmail.com>
2020-05-11 09:58:23 -0300

    continuação da implementação da classe pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,14 +3,22 @@
 
 class Pessoa:
     
+    # atributos de instancia
+    def __init__(self, nome=None, idade=35):
+        self.nome = nome
+        self.idade = idade 
+        
+    
     ##
     def cumprimentar(self):
         return f'Olá {id(self)}'
     
     
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('André')
+   
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
-    
+    print(p.nome)
+    print(p.idade)
