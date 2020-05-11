7078324ed0b31cbbf32bd596f9e55058e5ceebe8
andrerodriguesneves <andre.rodriguesneves@gmail.com>
2020-05-10 22:38:43 -0300

    Implementação da classe pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,5 +2,15 @@
 # toda classe deve comçar com letra maiuscula, se houver outra palavra, CamelCase
 
 class Pessoa:
-    pass
+    
+    ##
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+    
+    
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
     
